09c8b5ec2fe452e19dd87418785409041e1f0e01
Grant_Nathaniel_Brown <gnb225@nyu.edu>
2018-02-21 14:05:37 -0500

    part 3 also super easy after I studied it. Still working on that one mistake in part 1.

--- oojs.js
+++ oojs.js
@@ -5,7 +5,15 @@
 // - a `members` property that is an empty array not determined by input (not passed into the constructor)
 // - an `addMember` method that adds a person (string) to the `members` array
 // Type your solution immediately below this line:
-
+class Group {
+  constructor(name) {
+    this.name = name
+    this.members = []
+  }
+  addMember(person) {
+    this.members.push(person)
+  }
+}
 
 
 
@@ -13,7 +21,9 @@
 // #2: Create an instance of the Group class and set it to a variable called `classmates`
 // Call the instance's `addMember` method to add a person to the instance's `members` array
 // Type your solution immediately below this line:
-
+var classmates = new Group('wdiClass')
+classmates.addMember('Grant')
+console.log(classmates)
 
 
 
